7bdc9b857f8b2cbdbe277f2f984899c94e0341d4
Timothy Hanke <tim.hanke@gmail.com>
2020-11-19 22:49:46 -0500

    readying to deploy to vercel

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
   // API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_ENDPOINT: `https://hidden-plateau-97074.herokuapp.com/`,
   API_KEY: "ba9f34f8-1fcb-11eb-adc1-0242ac120002",
 };
