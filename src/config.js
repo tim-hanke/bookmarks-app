7829d43d1c0fd387c37051c20b3fed4654e4b1c0
Timothy Hanke <tim.hanke@gmail.com>
2020-11-22 09:00:04 -0500

    fixed API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   // API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
   // API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: `https://hidden-plateau-97074.herokuapp.com/`,
+  API_ENDPOINT: `https://hidden-plateau-97074.herokuapp.com/api/bookmarks/`,
   API_KEY: "ba9f34f8-1fcb-11eb-adc1-0242ac120002",
 };
